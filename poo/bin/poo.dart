8235b14c8b090c080d89c88a8dce22e8d230ebe7
Luiz Mendonça <luizeduardomendonca77@gmail.com>
2026-03-03 10:16:30 -0300

    feat: mixin poo
    aprofundamento em poo usando mixin, elemento único do Dart para adicionar comportamento a uma classe sem usar herança tradicional.

--- poo/bin/poo.dart
+++ poo/bin/poo.dart
@@ -1,21 +1,19 @@
 import 'package:poo/conta.dart';
 
 void main() {
-  Conta contaJoao = Conta('João', 500.0);
-  Conta contaThais = Conta('Thais', -2500.0);
-  ContaPoupanca contaPoupanca = ContaPoupanca('Maria', 1000.0, 0.05);
-  ContaCorrente contaCorrente = ContaCorrente('Carlos', 2000.0, 500.0);
+  ContaCorrente contaChris = ContaCorrente("Chris", 4000, 2);
+  ContaPoupanca contaDenize = ContaPoupanca("Denize", 4000, 2);
+  ContaEmpresa contaMatheus = ContaEmpresa("Matheus", 2000);
+  ContaInvestimento contaRoberta = ContaInvestimento("Roberta", 2000);
 
-  List<Conta> contas = [contaJoao, contaThais];
+  contaChris.imprimirSaldo();
+  contaChris.enviar(4300);
 
-  contaPoupanca.imprimirSaldo(contaPoupanca);
-  contaCorrente.imprimirSaldo(contaCorrente);
+  contaDenize.imprimirSaldo();
+  contaDenize.enviar(4300);
+  contaDenize.calculaRendimento();
+  contaDenize.imprimirSaldo();
 
-  print('-------------------------------------');
-
-  contaPoupanca.aplicarJuros();
-  contaCorrente.enviar(100.0);
-
-  contaPoupanca.imprimirSaldo(contaPoupanca);
-  contaCorrente.imprimirSaldo(contaCorrente);
+  contaMatheus.enviar(1000);
+  contaRoberta.receber(1000);
 }
--- poo/lib/conta.dart
+++ poo/lib/conta.dart
@@ -13,8 +13,8 @@ class Conta {
     }
   }
 
-  void imprimirSaldo(Conta conta) {
-    print('Saldo da conta de ${conta.nome}: ${conta._saldo}');
+  void imprimirSaldo() {
+    print('Saldo da conta de $nome: $_saldo');
   }
 
   String nome;
@@ -34,6 +34,8 @@ class ContaPoupanca extends Conta {
     receber(juros);
     print('Juros aplicados: $juros. Saldo atual: $_saldo');
   }
+
+  void calculaRendimento() {}
 }
 
 class ContaCorrente extends Conta {
@@ -52,5 +54,51 @@ class ContaCorrente extends Conta {
       _saldo -= valor;
       print('Valor enviado: $valor. Saldo atual: $_saldo');
     }
+  }
+}
+
+//mixin para codigo omitido
+
+mixin Imposto {
+  double taxa = 0.03;
+
+  double valorTaxado(double valor) {
+    return valor * taxa;
+  }
+}
+
+class ContaEmpresa extends Conta with Imposto {
+  ContaEmpresa(super.titular, super._saldo);
+
+  @override
+  void enviar(double valor) {
+    if (_saldo >= valor + valorTaxado(valor)) {
+      _saldo -= valor + valorTaxado(valor);
+      imprimirSaldo();
+    }
+  }
+
+  @override
+  void receber(double valor) {
+    _saldo += valor - valorTaxado(valor);
+    imprimirSaldo();
+  }
+}
+
+class ContaInvestimento extends Conta with Imposto {
+  ContaInvestimento(super.titular, super._saldo);
+
+  @override
+  void enviar(double valor) {
+    if (_saldo >= valor + valorTaxado(valor)) {
+      _saldo -= valor + valorTaxado(valor);
+      imprimirSaldo();
+    }
+  }
+
+  @override
+  void receber(double valor) {
+    _saldo += valor - valorTaxado(valor);
+    imprimirSaldo();
   }
 }
